87a140e8bc4a26a9bf36ec8e4a36dd2bdd29b41f
ZANIVAN <3262287@ppti-14-407-05.ufr-info-p6.jussieu.fr>
2018-05-18 16:23:53 +0200

    adebug

--- Utils/utils.py
+++ Utils/utils.py
@@ -15,7 +15,7 @@ def convert_rgb_to_hsv(img):
 def display_im(im):
     #Display a matrice im from an image
     plt.figure(figsize=(12, 8))
-    plt.imshow(im)
+    plt.imshow(im, interpolation='nearest')
     plt.show
 
 def get_patch(i,j,h,im):
@@ -31,6 +31,25 @@ def get_patch(i,j,h,im):
         return im[(i-h//2):(i+h//2), (j-h//2):(j+h//2)]
     else:
         return im[(i-h//2):(i+h//2)+1, (j-h//2):(j+h//2)+1]
+    
+#fonction qui gère les bordures (auquel cas les patchs ne sont pas centrés)
+def get_noisy_patch(i,j,h,im):
+    width = im.shape[0]
+    height = im.shape[1]
+    
+    #todo gérer coins
+    
+    #bordure gauche
+        
+    #bordure droite
+    
+    #bordure haute
+    
+    #bordure bas
+        
+    #point normal
+    return get_patch(i,j,h,im)
+    
 
 def convert_patch_into_vectors(patch):
     pass
@@ -40,17 +59,28 @@ def flatpixel_to_2d(flatpixel, width):
     y = int((flatpixel - (flatpixel%width))/width)
     return (x,y)
     
+
 #return flatimg and pixels of noise_pixels in 2d
 def noise(img,prc,width,height, h):
     #Noise a prc percent of the image img 
     flatimg = img.reshape(-1, img.shape[-1])
     randnoise = random.sample(range(0, len(flatimg)), int(len(flatimg)*prc))
-    for i in randnoise:
-        flatimg[i] = np.array([0,0,0])
-    return (flatimg.reshape([img.shape[0], img.shape[1], 3]), np.array([flatpixel_to_2d(x,width) for x in randnoise]))
+    noised_pixels = []
+    for pixeli in randnoise:
+        i,j = flatpixel_to_2d(pixeli,width)
+        #test si c'est sur un bord on fait rien
+        if((i-h >= 0) and (j-h >= 0) and (i+h+1 <= width) and (j+h+1 <= height)):
+            #sinon on met du noise
+            flatimg[pixeli] = np.array([0,0,0])  #noir
+            #flatimg[pixeli] = np.array([255,255,255]) #blanc
+            noised_pixels.append(flatpixel_to_2d(pixeli,width))
+        else:
+            print("pixel ", i, " ",j, "=(",pixeli,") is on border")
+            pass
+    return (flatimg.reshape([img.shape[0], img.shape[1], 3]), np.array(noised_pixels))
 
     
-def delete_rect(img,i,j,height, width):
+def delete_rect(img,i,j, width, height):
     #Delete a rectangle of an image centered on i, j and size height and width
     img[(i-height//2):(i+height//2), (j-width//2):(j+width//2)] = np.zeros((height, width, 3))
     return img
@@ -62,8 +92,25 @@ def check_if_noisy_patch(patch):
             if(np.array_equal(y, np.array([0, 0, 0]))):
                 return True
     return False
+
+#version quadrillage
+def get_patches(img, h, width, height):
+    #Return a tuple wich contains the noisy patches and the clean patches (without noisy pixel)
+    noisy_patches = []
+    clean_patches = []
     
-def get_patches(img, h, height, width):
+    for i_height in range(h//2, height-h//2,h):
+        for j_width in range(h//2, width-h//2,h):
+            patch = get_patch(i_height, j_width, h, img)
+            if(check_if_noisy_patch(patch)):
+                noisy_patches.append(patch)
+            else:
+                clean_patches.append(patch)
+                
+    return (noisy_patches, clean_patches)
+
+#version sans quadrillage
+def get_all_patches(img, h,width, height):
     #Return a tuple wich contains the noisy patches and the clean patches (without noisy pixel)
     noisy_patches = []
     clean_patches = []
